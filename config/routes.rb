edd989fd918dea1fb7b180d8230829e59c1241f3
yuto-takami <orca.y1322@gmail.com>
2021-08-22 14:01:12 +0000

    クリッカブルマップ実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,20 +2,21 @@ Rails.application.routes.draw do
   get 'search/search'
   devise_for :users
   root to: 'homes#top'
+  get "home/map" => "homes#map"
   get '/search' => 'search#search'
 
   resources :reviews do
-    resource :favorites, only: %i[create destroy]
+    resource :favorites, only: [:create, :destroy]
 
-    resources :review_comments, only: %i[create destroy]
+    resources :review_comments, only: [:create, :destroy]
   end
 
   resources :tags do
     get 'reviews', to: 'reviews#search'
   end
 
-  resources :users, only: %i[show index edit update] do
-    resource :relationships, only: %i[create destroy]
+  resources :users, only: [:show, :index, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
   end
